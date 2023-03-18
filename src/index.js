1708cc1dc7910e1b8c4ab56977d986ac2e5c70e3
CodeSandbox <support@codesandbox.io>
2023-03-18 06:34:18 +0000

    "Initial commit"

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { App as Canvas } from './Canvas'
+import { Overlay } from './Overlay'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <Canvas />
+    <Overlay />
+  </>
+)
